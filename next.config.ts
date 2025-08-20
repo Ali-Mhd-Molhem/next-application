d5ac696d91b3a494fa0abf41aabfc5a251fadba9
Ali Molhem <a.molhem@hardwework.com>
2025-08-20 14:17:42 +0400

    add a blog link to test the zone

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  // output: "standalone",
+
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: `http://localhost:4009/blog`,
+      },
+      {
+        source: "/blog/:path+",
+        destination: `http://localhost:4009/blog/:path+`,
+      },
+      {
+        source: "/blog-static/_next/:path+",
+        destination: `http://localhost:4009/blog-static/_next/:path+`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
